1ddd3db2aad39343ba185c0eed01ff5e38e74dbc
Salvador <alum.smunozc@iesalixar.org>
2020-05-19 21:29:09 +0200

    Funcion carrousel terminada

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,35 @@
-let fotosSrc = ['../../img/imagen1.png','../../img/imagen2.png'];
-    cont = 0;
-let funcionCarrousel = function(carrousel){
-    carrousel
+let imagenes = ['../../img/imagen1.jpg','../../img/imagen2.jpg'];
+let contador = 0;
+
+function carrousel(direccion){
+    let direccion1 = direccion;
+
+    if(direccion1=='atras'){
+        if(contador==0){
+            document.getElementById('imagen1').src= imagenes[imagenes.length-1];
+            contador++;
+        }else if (contador<imagenes.length-1) {
+            document.getElementById('imagen1').src= imagenes[imagenes.length-1-contador];
+            contador++;
+
+        }else {
+            document.getElementById('imagen1').src= imagenes[0];
+            contador=0;
+        }
+    }
+
+    if(direccion1=='siguiente'){
+        if(contador==0){
+            document.getElementById('imagen1').src= imagenes[0];
+            contador++;
+        }else if (contador<imagenes.length-1) {
+            document.getElementById('imagen1').src= imagenes[contador];
+            contador++;
+
+        }else {
+            document.getElementById('imagen1').src= imagenes[imagenes.length-1];
+            contador=0;
+        }
+    }
+
 }
