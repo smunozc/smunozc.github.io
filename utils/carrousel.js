252dd7c8bed3348b85b26b3c62d23fb5a40a0a9c
Salvi <salvador.munoz0109@gmail.com>
2020-06-19 01:04:45 +0200

    Cambios en el carrousel, aún está por mejorar, añadida imagen

--- utils/carrousel.js
+++ utils/carrousel.js
@@ -1,4 +1,4 @@
-let imagenes = ['../../img/imagen1.jpg','../../img/imagen2.jpg', '../../img/imagen3.jpg'];
+let imagenes = ['../../img/imagen1.jpg','../../img/imagen2.jpg', '../../img/imagen3.jpg', '../../img/imagen4.jpg'];
 let contador = 0;
 
 function carrousel(direccion){
@@ -6,28 +6,28 @@ function carrousel(direccion){
 
     if(direccion1=='atras'){
         if(contador==0){
-            document.getElementById('imagen1').src= imagenes[imagenes.length-1];
+            document.getElementById('imagen').src= imagenes[imagenes.length-1];
             contador++;
         }else if (contador<imagenes.length-1) {
-            document.getElementById('imagen1').src= imagenes[imagenes.length-1-contador];
+            document.getElementById('imagen').src= imagenes[imagenes.length-1-contador];
             contador++;
 
         }else {
-            document.getElementById('imagen1').src= imagenes[0];
+            document.getElementById('imagen').src= imagenes[0];
             contador=0;
         }
     }
 
     if(direccion1=='siguiente'){
         if(contador==0){
-            document.getElementById('imagen1').src= imagenes[0];
+            document.getElementById('imagen').src= imagenes[0];
             contador++;
         }else if (contador<imagenes.length-1) {
-            document.getElementById('imagen1').src= imagenes[contador];
+            document.getElementById('imagen').src= imagenes[contador];
             contador++;
 
         }else {
-            document.getElementById('imagen1').src= imagenes[imagenes.length-1];
+            document.getElementById('imagen').src= imagenes[imagenes.length-1];
             contador=0;
         }
     }
