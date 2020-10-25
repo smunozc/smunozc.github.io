18b4ef6f306ce7361323839afc90c9adae05cfaf
salvi <salvador.munoz0109@gmail.com>
2020-10-25 21:30:44 +0100

    arreglo del carrusel

--- utils/carrousel.js
+++ utils/carrousel.js
@@ -1,34 +1,26 @@
-let imagenes = ['../../img/imagen1.jpg','../../img/imagen2.jpg', '../../img/imagen3.jpg', '../../img/imagen4.jpg'];
+let imagenes = ['../../img/imagen1.jpg', '../../img/imagen2.jpg', '../../img/imagen3.jpg', '../../img/imagen4.jpg'];
 let contador = 0;
 
-function carrousel(direccion){
+function carrousel(direccion) {
     let direccion1 = direccion;
 
-    if(direccion1=='atras'){
-        if(contador==0){
-            document.getElementById('imagen').src= imagenes[imagenes.length-1];
-            contador++;
-        }else if (contador<imagenes.length-1) {
-            document.getElementById('imagen').src= imagenes[imagenes.length-1-contador];
-            contador++;
-
-        }else {
-            document.getElementById('imagen').src= imagenes[0];
-            contador=0;
+    if (direccion1 === 'atras') {
+        if (contador === 0) {
+            document.getElementById('imagen').src = imagenes[imagenes.length - 1];
+            contador = imagenes.length - 1;
+        } else if (contador > 0 && contador <= imagenes.length - 1) {
+            contador--;
+            document.getElementById('imagen').src = imagenes[contador];
         }
     }
 
-    if(direccion1=='siguiente'){
-        if(contador==0){
-            document.getElementById('imagen').src= imagenes[0];
+    if (direccion1 === 'siguiente') {
+        if (contador >= 0 && contador < imagenes.length-1) {
             contador++;
-        }else if (contador<imagenes.length-1) {
-            document.getElementById('imagen').src= imagenes[contador];
-            contador++;
-
-        }else {
-            document.getElementById('imagen').src= imagenes[imagenes.length-1];
-            contador=0;
+            document.getElementById('imagen').src = imagenes[contador];
+        } else if(contador === imagenes.length -1){
+            contador = 0;
+            document.getElementById('imagen').src = imagenes[contador];
         }
     }
 
