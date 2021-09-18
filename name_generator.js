fc4162be2631faf1146b641b1f99cab02554226d
smunozc <salvador.munoz0109@gmail.com>
2021-09-19 01:21:23 +0200

    Update

--- name_generator.js
+++ name_generator.js
@@ -1,48 +1,72 @@
 let pButton = document.getElementById('pButton');
-pButton.addEventListener('click',()=>{
+let count = 0;
+
+pButton.addEventListener('click', () => {
     let realName = document.getElementById('nameGenerator_realName').value;
-    let display = document.getElementById('nameGenerator_display');
-    let nameType = document.getElementById('nameGenerator_nameType').value;
-    let endName = "";
 
-    let RUS_endings = ['nik','sko','mir'];
-    let ESP_endings = ['ez','oz','ro'];
-    let vocals = ['a','e','i','o','u'];
+    if (realName.length > 1) {
+        let display = document.getElementById('nameGenerator_display');
+        let nameType = document.getElementById('nameGenerator_nameType').value;
+        let endName = "";
 
-    let RUS_selector = Math.floor(Math.random() * (RUS_endings.length - 0)) + 0;
-    let ESP_selector = Math.floor(Math.random() * (ESP_endings.length - 0)) + 0;
+        if (realName != null && realName.length > 0) {
+            count++;
+        }
 
-    switch (nameType) {
-        case 'rus':
-            for (let i = 0; i < realName.length; i++) {
-                if(i >= realName.length/2) {
-                    for (let j = 0; j < realName.length; j++) {
-                        if(realName.charAt(i) == vocals[j]){
-                            endName = realName.substring(0, i + 1);
-                            endName += RUS_endings[RUS_selector];
+        // A little joke
+        if (count == 10) {
+            if (confirm("You're bored too, right " + realName + "?")) {
+                alert("Keep it going then :)");
+            } else {
+                alert("Sure...");
+            }
+        }
+
+        let RUS_endings = ['nik', 'sko', 'mir'];
+        let ESP_endings = ['ez', 'oz', 'ro'];
+        let vocals = ['a', 'e', 'i', 'o', 'u'];
+
+        let RUS_selector = Math.floor(Math.random() * (RUS_endings.length - 0)) + 0;
+        let ESP_selector = Math.floor(Math.random() * (ESP_endings.length - 0)) + 0;
+        let isFinished = false;
+
+        switch (nameType) {
+            case 'rus':
+                for (let i = 0; i < realName.length && !isFinished; i++) {
+                    if (i >= (realName.length / 2) - 1) {
+                        for (let j = 0; j < vocals.length; j++) {
+                            if (realName.charAt(i) == vocals[j]) {
+                                endName = realName.substring(0, i + 1);
+                                endName += RUS_endings[RUS_selector];
+                                isFinished = true;
+                            }
                         }
                     }
                 }
-            }
-            break;
-    
-        case 'esp':
-            for (let i = 0; i < realName.length; i++) {
-                if(i >= realName.length/2) {
-                    for (let j = 0; j < realName.length; j++) {
-                        if(realName.charAt(i) == vocals[j]){
-                            endName = realName.substring(0, i + 1);
-                            endName += ESP_endings[ESP_selector];
+                break;
+
+            case 'esp':
+                for (let i = 0; i < realName.length && !isFinished; i++) {
+                    if (i >= (realName.length / 2) - 1) {
+                        for (let j = 0; j < vocals.length; j++) {
+                            if (realName.charAt(i) == vocals[j]) {
+                                endName = realName.substring(0, i + 1);
+                                endName += ESP_endings[ESP_selector];
+                                isFinished = true;
+                            }
                         }
                     }
                 }
-            }
-            break;
+                break;
 
-        default:
-            endName = "WTF";
-            break;
+            default:
+                endName = "WTF";
+                break;
+        }
+
+        display.innerHTML = endName;
+    } else {
+        alert("Name must be at least 2 characters long");
     }
 
-    display.innerHTML = endName;
 });
